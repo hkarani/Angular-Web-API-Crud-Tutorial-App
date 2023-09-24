cb2865726d8599de45d8a0e0c521844c34b85f0c
hkarani <hezronkaranii@gmail.com>
2023-09-24 15:44:48 +0300

    SuperHero Model and API

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Controllers
+{
+	[Route("/api/[controller]")]
+	[ApiController]
+
+	public class SuperHeroController : Controller
+	{
+		[HttpGet]
+		public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+		{
+			return new List<SuperHero>
+			{
+				new SuperHero
+				{
+					Name= "SpiderMan",
+					FirstName = "Peter",
+					LastName = "Parker",
+					Place = "New York City"
+				}
+			};
+		}
+	}
+}
